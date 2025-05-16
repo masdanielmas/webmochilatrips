826b49bd9d581fee4931c45d3f59f80709725c5b
Maka-1019 <jkamila.amaya10@gmail.com>
2025-05-15 23:26:57 -0500

    Se cambia link por href en cada html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { DestinosComponent } from './components/destinos/destinos.component';
+import { PlanesComponent } from './components/planes/planes.component';
 
 export const routes: Routes = [
     {path: 'inicio', component: InicioComponent},
-    {path: 'destinos', component: DestinosComponent}
+    {path: 'destinos', component: DestinosComponent},
+    {path: 'planes', component: PlanesComponent}
 ];
 
--- src/app/components/planes/planes.component.ts
+++ src/app/components/planes/planes.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-planes',
   imports: [],
-  templateUrl: './planes.component.css',
-  styleUrl: './planes.component.html'
+  templateUrl: './planes.component.html',
+  styleUrl: './planes.component.css'
 })
 export class PlanesComponent {
 
