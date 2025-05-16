b09cd22472fe86081326c03f41ccfa9c86f7b8d6
Marlon0927 <marlonr.gomezskonradlorenz.edu.co>
2025-05-15 23:29:52 -0500

    se agrega modulo reservas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './components/inicio/inicio.component';
 import { DestinosComponent } from './components/destinos/destinos.component';
 import { PlanesComponent } from './components/planes/planes.component';
+import { ReservasComponent } from './components/reservas/reservas.component';
 
 export const routes: Routes = [
     {path: 'inicio', component: InicioComponent},
     {path: 'destinos', component: DestinosComponent},
-    {path: 'planes', component: PlanesComponent}
+    {path: 'planes', component: PlanesComponent},
+    {path: 'reservas', component: ReservasComponent}
 ];
 
