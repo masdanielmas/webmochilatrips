c18eae3815fecaea58550750b605354799cdd4de
Marlon0927 <marlonr.gomezskonradlorenz.edu.co>
2025-05-20 21:44:02 -0500

    Se agrega login y registrar usuario (html)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { DestinosComponent } from './components/destinos/destinos.component';
 import { PlanesComponent } from './components/planes/planes.component';
 import { ReservasComponent } from './components/reservas/reservas.component';
 import { UserComponent } from './components/user/user.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
     {path: 'inicio', component: InicioComponent},
     {path: 'destinos', component: DestinosComponent},
     {path: 'planes', component: PlanesComponent},
     {path: 'reservas', component: ReservasComponent},
-    {path: 'user', component: UserComponent}
+    {path: 'user', component: UserComponent},
+    {path: 'login', component: LoginComponent}
 ];
 
