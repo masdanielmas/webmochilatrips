eebc24f2f9de258d3ee371ef3a48f85d436eb01f
Marlon0927 <marlonr.gomezskonradlorenz.edu.co>
2025-05-23 22:54:23 -0500

    creado el form de registro usuarios (no funcional)

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../../services/user.service';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { take } from 'rxjs';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, FormsModule],
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
@@ -13,8 +17,9 @@ export class UserComponent implements OnInit {
 
 
   userList: any[] = [];
-
-  constructor(private userService: UserService) { }
+  userForm: FormGroup | any;
+  constructor(private userService: UserService, private formBuilder: FormBuilder,
+    private router: Router) { }
 
   ngOnInit() {
     this.getAllUsers();
@@ -26,6 +31,17 @@ export class UserComponent implements OnInit {
     });
   }
 
-  
+  newUserEntry() {
+    this.userService.newUser(this.userForm.value).subscribe(
+      () => {
+        //Redirigiendo a la ruta actual /inicio y recargando la ventana
+        this.router.navigate(['/user'])
+          .then(() => {
+            
+          })
+      }
+    );
+  }
+
 
 }
